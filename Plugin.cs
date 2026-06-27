1733dbff7cce022d9bff98754ba21aa9141f4737
Black_Moss <kytheminecraft@outlook.com>
2026-06-27 20:34:29 +0800

    1.1.0

--- Plugin.cs
+++ Plugin.cs
@@ -7,9 +7,9 @@ namespace MossTemplate;
 [BepInPlugin(Guid, Name, Version)]
 public class Plugin : BaseUnityPlugin
 {
-    public const string Guid = "org.explosivehydra.mosstemplate";
+    public const string Guid = "org.cncumc.mosstemplate";
     public const string Name = "Moss Template";
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.0";
     internal new static ManualLogSource Logger;
     private readonly Harmony _harmony = new(Guid);
 
