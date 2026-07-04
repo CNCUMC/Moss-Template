7d038a4aaa2f28b6f1bb268d88f0647406a2837d
Black_Moss <kytheminecraft@outlook.com>
2026-07-05 03:55:33 +0800

    logo

--- Release.ps1
+++ Release.ps1
@@ -187,7 +187,7 @@ if (Test-Path $dllSource) {
 }
 
 # 文档文件 (含更新日志)
-$docFiles = @("README.md", "README_ZH.md", "LICENSE.md", "CHANGELOG.md", "CHANGELOG_ZH.md", "Cover.png")
+$docFiles = @("README.md", "README_ZH.md", "LICENSE.md", "CHANGELOG.md", "CHANGELOG_ZH.md")
 foreach ($doc in $docFiles) {
     $docPath = Join-Path $scriptDir $doc
     if (Test-Path $docPath) {
